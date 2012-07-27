2bb9762c35c2c8e9e2ea22ecb1caf0ee6df85369
Marc Litchfield <marclitchfield@gmail.com>
2012-07-26 23:45:08 -0700

    Set up hello server with nodejitsu

--- helloworld/bin/hello.js
+++ helloworld/bin/hello.js
@@ -1,8 +1,8 @@
+#!/usr/bin/env node
+var port = process.env.PORT || 8080;
 var http = require('http');
 http.createServer(function (req, res) {
 	res.writeHead(200, {'Content-Type': 'text/plain'});
   res.end('Hello World\n');
-}).listen(1337, '127.0.0.1');
-console.log('Server running at http://127.0.0.1:1337/');
-
-
+}).listen(port);
+console.log('Server running at http://127.0.0.1:' + port + '/');
