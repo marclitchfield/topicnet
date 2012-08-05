99700b7c5aa0fb6fee327f99764b3acfe263c77f
Marc Litchfield <marclitchfield@gmail.com>
2012-08-05 13:10:37 -0700

    Started implementing the link method. Doesn't work yet

--- server.js
+++ server.js
@@ -1,5 +1,6 @@
 var express = require('express');
 var neo4j = require('neo4j');
+var _ = require('underscore');
 
 var app = express.createServer(express.logger());
 var graph = new neo4j.GraphDatabase('http://localhost:7474');
@@ -15,6 +16,15 @@ app.post('/node/', function(request, response) {
 app.get('/topic/:id/links', function(request, response) {
 });
 
+app.get('/topic', function(request, response) {
+	graph.getNodeById(0, function(root) {
+		var rootNodes = _.map(root.all, function(node) {
+			return { name: node.name };
+		});
+		response.json(rootNodes);
+	});
+});
+
 app.post('/topic', function(request, response) {
 	var data  = { 'name': request.body.name };
 	var node = graph.createNode(data);
@@ -25,6 +35,23 @@ app.post('/topic', function(request, response) {
 	}, function(err) {
 		console.log('Error saving node: ' + data.name + ', ' + err);
 	})
+});
+
+app.post('/link', function(request, response) {
+	graph.getNodeById(request.body.from, function(fromNode) {
+		graph.getNodeById(request.body.to, function(toNode) {
+			var relationshipType = request.body.type;
+
+			fromNode.createRelationship(toNode, relationshipType, function() {
+				console.log('Created relationship ' + 
+						request.body.from + '-[ ' + 
+						request.body.type + ' ]-> ' + request.body.to);
+
+			}, function(err) {
+				console.log('Error saving relationship: ' + err);
+			});
+		});
+	});
 });
 
 var port = process.env.PORT || 5000;
