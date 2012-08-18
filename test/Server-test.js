df16157d512d1183ab6371f9b65a62249d7ec930
Marc Litchfield <marclitchfield@gmail.com>
2012-08-17 22:50:01 -0700

    Configure to output the spec. Format the tests better

--- test/Server-test.js
+++ test/Server-test.js
@@ -3,12 +3,11 @@ var APIeasy = require('api-easy'),
 
 var suite = APIeasy.describe('your/awesome/api');
 
-suite.discuss('When using your awesome API')
-     .discuss('and your awesome resource')
-     .use('localhost', 5000)
-	 .path('/topic')
-     .setHeader('Content-Type', 'application/json')
-     .post({ name: 'testnode' })
-		.expect(200)
- 	    .expect({ "name": "testnode" })
+suite.discuss('When using the API')
+	.use('localhost', 5000)
+	.path('/topic')
+    .setHeader('Content-Type', 'application/json')
+    	.post({ name: 'testnode' })
+			.expect(200)
+ 	    	.expect({ "name": "testnode" })
 	.export(module);
