84476f6d9fdf28bf4c2b97b64ef161760e20c9e3
Marc Litchfield <marclitchfield@gmail.com>
2012-08-17 23:49:17 -0700

    Change javascript to use relative path. can't hardcode localhost on heroku

--- public/add.js
+++ public/add.js
@@ -2,7 +2,7 @@ $(function() {
 	$("#submit").click(function() {
 		var param = { name: $("#name").val() };
 
-		$.post('http://localhost:5000/topic/', param, function(node) {
+		$.post('/topic/', param, function(node) {
 				$("#name").val("");
 				var message = "<p>Node Created: " + node.name + "</p>";
 				$("#add-node-form").after(message);
