05ee6c091a9c08c93885dbe597ed07d413d45c3f
Marc Litchfield <marclitchfield@gmail.com>
2012-08-17 22:23:52 -0700

    Got a passing API easy test!

--- test/Server-test.js
+++ test/Server-test.js
@@ -6,10 +6,9 @@ var suite = APIeasy.describe('your/awesome/api');
 suite.discuss('When using your awesome API')
      .discuss('and your awesome resource')
      .use('localhost', 5000)
+	 .path('/topic')
      .setHeader('Content-Type', 'application/json')
      .post({ name: 'testnode' })
- 	    .expect(200, { ok: true })
-		.expect('should respond with new node', function (err, res, body) {
-        	assert.equal(res.body, '{ "name": "testnode" }');
-         })
+		.expect(200)
+ 	    .expect({ "name": "testnode" })
 	.export(module);
