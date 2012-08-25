19f53857d16d1add15692d2d8507948dd50f70e5
Marc Litchfield <marclitchfield@gmail.com>
2012-08-24 22:54:49 -0700

    Removed APIEasy for now, and refactored tests to use vows. Changed TopicService to return the id of the created topic

--- test/Server-test.js
+++ test/Server-test.js
@@ -1,18 +1,55 @@
-var APIeasy = require('api-easy'),
-    assert = require('assert');
+var assert = require('assert'),
+	request = require('request'),
+	vows = require('vows');
 
-var suite = APIeasy.describe('artoplasm REST API');
+var api = {
+	post: function(path, body, callback) {
+		request({
+			uri: 'http://localhost:5000' + path,
+			method: 'POST',
+			body: JSON.stringify(body),
+			headers: { 'Content-Type': 'application/json' }
+		}, callback);
+	},
+	get: function(path, callback) {
+		request({
+			uri: 'http://localhost:5000' + path,
+			method: 'GET',
+			headers: { 'Content-Type': 'application/json' }
+		}, callback);
+	}
+}; 
+			
 
-suite.discuss('When using the API')
-	.use('localhost', 5000)
-	.path('/topic')
-    .setHeader('Content-Type', 'application/json')
-    	.post({ name: 'testnode' })
-			.expect(200)
- 	    	.expect({ "name": "testnode" })
-		.post({  })
-			.expect(500)
-			.expect('should respond with error', function(err, res, body) {
-				assert.include(res.body, 'name is required');
-			})
-	.export(module);
+vows.describe('/topic').addBatch({
+	'POST to /topic': {
+		topic: function() { 
+			api.post('/topic', { name: 'testnode' }, this.callback);
+		},
+		'returns status code 200 OK': function( err, res ) {
+			assert.equal(res.statusCode, 200);
+		},
+		'responds with new topic object': {
+			topic: function(res) { return JSON.parse(res.body); },
+
+			'with a name': function( obj ) {
+				assert.equal(obj.name, 'testnode');
+			},
+			'and a valid id': function( obj ) {
+				assert.ok( obj.id > 0 );
+			}
+		}	
+	},
+	'POST to /topic with no name': {
+		topic: function() {
+			api.post('/topic', {}, this.callback);
+		},
+		'returns status code 500': function( err, res ) {
+			assert.equal(res.statusCode, 500);
+		},
+		'responds with error message': function( err, res ) {
+			assert.include(res.body, 'name is required');
+		}
+	}
+}).export(module);
+				
