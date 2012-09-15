40e785c5843ab18492add60bb3905503bb01fc5b
Marc Litchfield <marclitchfield@gmail.com>
2012-09-15 15:02:04 -0700

    Add root, sub, and next topics. Creates a new topic

--- public/js/controllers.js
+++ public/js/controllers.js
@@ -10,10 +10,40 @@ function RootController($scope, $http) {
 	$http.get('/topics').success(function(topics) {
 		$scope.rootTopics = topics;
 	});
+
+	$scope.addRootTopic = function() {
+		$http.post('/topics', { name: $scope.rootTopicName }).success(function(newTopic) {
+			$http.post('/topics/' + newTopic.id + '/root').success(function() {
+				$scope.rootTopics.push(newTopic);
+				$scope.rootTopicName = '';
+			});
+		});
+	};
 }
 
 function DetailController($scope, $http, $routeParams) {
 	$http.get('/topics/' + $routeParams.topicId).success(function(topic) {
 		$scope.topic = topic;
+		$scope.subTopics = topic.sub || [];
+		$scope.nextTopics = topic.next || [];
+		$scope.resources = topic.resources || [];
 	});
+
+	$scope.addSubTopic = function() {
+		$http.post('/topics', { name: $scope.subTopicName }).success(function(newTopic) {
+			$http.post('/topics/' + $scope.topic.id + '/sub', { toid: newTopic.id }).success(function() {
+				$scope.subTopics.push(newTopic);
+				$scope.subTopicName = '';
+			});
+		});
+	};
+
+	$scope.addNextTopic = function() {
+		$http.post('/topics', { name: $scope.nextTopicName }).success(function(newTopic) {
+			$http.post('/topics/' + $scope.topic.id + '/next', { toid: newTopic.id }).success(function() {
+				$scope.nextTopics.push(newTopic);
+				$scope.nextTopicName = '';
+			});
+		});
+	};
 }
