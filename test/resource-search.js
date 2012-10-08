d4942312312ee71c342fe15268358b988770c151
David Rueck <drueck@gmail.com>
2012-10-08 00:04:00 -0700

    Added tests for resource search by title and url including some lucene special chars in the queries

---
+++ test/resource-search.js
@@ -0,0 +1,70 @@
+var assert = require('assert');
+var _ = require('underscore');
+var api = require('./helper-api.js');
+
+describe('Resource Search', function() {
+
+	var resourceToFind;
+
+	before(function(done) {
+		api.post('/resources', { title: 'resource to find!', url: 'http://example.com', source: 'example.com' }, 
+			function(err, res) {
+			resourceToFind = JSON.parse(res.body);
+			done(err);
+		})
+	})
+
+	describe('GET /resources?invalid', function() {
+
+		it('returns status 404', function(done) {
+			api.get('/resources?invalid', function(err, res) {
+				assert.equal(res.statusCode, 404);
+				done();
+			});
+		})
+
+	})
+
+	describe('GET /resources?title with substring', function() {
+
+		it('returns existing resource', function(done) {
+			api.get('/resources?title=ource t', function(err, res) {
+				var searchResults = JSON.parse(res.body);
+				assert.ok(_.any(searchResults, function(r) {
+					return r.id === resourceToFind.id;
+				}));
+				done();
+			});
+		})
+
+	})
+
+	describe('GET /resources?title with ! in query', function() {
+
+		it('returns existing resource', function(done) {
+			api.get('/resources?title=nd!', function(err, res) {
+				var searchResults = JSON.parse(res.body);
+				assert.ok(_.any(searchResults, function(r) {
+					return r.id === resourceToFind.id;
+				}));
+				done();
+			});
+		})
+
+	})
+
+	describe('GET /resources?url with substring including : character', function() {
+
+		it('returns existing resource', function(done) {
+			api.get('/resources?url=' + encodeURIComponent('://examp'), function(err, res) {
+				var searchResults = JSON.parse(res.body);
+				assert.ok(_.any(searchResults, function(r) {
+					return r.id === resourceToFind.id;
+				}));	
+				done();
+			});
+		})
+
+	})
+
+})
