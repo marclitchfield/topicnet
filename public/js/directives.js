457b185d214744a880d3336ca52ab305b7eb5296
Marc Litchfield <marclitchfield@gmail.com>
2012-10-13 08:35:24 -0700

    Figured out how to set the input for a modal. Need to use a directive

--- public/js/directives.js
+++ public/js/directives.js
@@ -37,5 +37,13 @@ angular.module('artoplasm.directives', []).
 				}
 			});
 		};
+	}).
+
+	directive('modalAutofocus', function() {
+		return function(scope, element, attrs) {
+			$(element).on('shown', function() {
+				$(this).find('[autofocus]').focus();
+			});
+		};
 	});
 
