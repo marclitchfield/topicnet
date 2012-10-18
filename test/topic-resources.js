e4ae58272eac0cd5f101ad3ddcb4acc491f323d6
David Rueck <drueck@gmail.com>
2012-10-18 02:31:24 -0700

    Implemented DELETE /topics/:id/resources to unlink a resource from a topic.

--- test/topic-resources.js
+++ test/topic-resources.js
@@ -68,4 +68,79 @@ describe('Topic Resources', function() {
 
 	});
 
+	describe('DELETE /topics/:id/resources with invalid id', function() {
+
+		it('returns status 404', function(done) {
+			api.del('/topics/-9999999/resources', {}, function(err, res) {
+				assert.equal(res.statusCode, 404);
+				done();
+			});
+		});
+
+	});
+
+	describe('DELETE /topics/:id/resources with invalid resid', function() {
+
+		var p = api.request();
+		
+		before(function(done) {
+			p.postTopic(done);
+		});
+
+		it('returns status 404', function(done) {
+			api.del('/topics/' + p.returnedTopic.id + '/resources', { resid: -9999999 }, 
+				function(err, res) {
+					assert.equal(res.statusCode, 404);
+					done();
+				}
+			);
+		});
+
+	});
+
+	describe('DELETE /topics/:id/resources with valid data', function() {
+
+		var topicPost = api.request();
+		var resourcePost = api.request();
+
+		before(function(done) {
+			topicPost.postTopic(function() {
+				resourcePost.postResource(function() {
+					api.post('/topics/' + topicPost.returnedTopic.id + '/resources',
+						{ resid: resourcePost.returnedResource.id },
+						function(err, res) {
+							done();
+						}
+					);
+				});
+			});
+		});
+
+		it('returns status 200', function(done) {
+			api.del('/topics/' + topicPost.returnedTopic.id + '/resources',
+				{ resid: resourcePost.returnedResource.id },
+				function(err, res) {
+					assert.equal(res.statusCode, 200);
+					done();
+				}
+			);
+		});
+
+		describe('then GET /topics/:id', function() {
+
+			it('does not include the unlinked resource', function(done) {
+				var g = api.request();
+				g.getTopic(topicPost.returnedTopic.id, function() {
+					assert.ok(g.returnedTopic.resources === undefined || 
+						!_.any(g.returnedTopic.resources, function(r) {
+							return r.id === resourcePost.returnedResource.id;
+						}));
+					done();		
+				});
+			});
+
+		});
+
+	});
+
 });
