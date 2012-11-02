40a0a113c1a197a9a1dfe408824a2e9de3e4a25d
David Rueck <drueck@gmail.com>
2012-11-02 01:43:36 -0700

    Modified topic search test criteria to test for matches returned, not necessarily specific a specific node to deal with paginated results.

--- test/topic-search.js
+++ test/topic-search.js
@@ -15,35 +15,37 @@ describe('Topic Search', function() {
 	});
 
 	describe('GET /topics?q with substring', function() {
-		var searchResults;
+    var searchString = 'estnod';
+    var searchResults;
 
 		before(function(done) {
-			api.get('/topics?q=estnod', function(err, res) {
+			api.get('/topics?q=' + searchString, function(err, res) {
 				searchResults = JSON.parse(res.body);
 				done();
 			});
 		});
 
-		it('returns existing topic', function() {
+		it('returns a topic containing the search string', function() {
 			assert.ok(_.any(searchResults, function(t) {
-				return t.id === topicToFind.id;
+        return t.name.indexOf(searchString) !== -1;
 			}));
 		});
 	});
 
 	describe('GET /topics?q with spaces', function() {
+    var searchString = 'testnode to f';
 		var searchResults;
 
 		before(function(done) {
-			api.get('/topics?q=testnode to f', function(err, res) {
+			api.get('/topics?q=' + searchString, function(err, res) {
 				searchResults = JSON.parse(res.body);
 				done();
 			});
 		});
 
-		it('returns existing topic', function() {
+		it('returns a topic containing the search string', function() {
 			assert.ok(_.any(searchResults, function(t) {
-				return t.id === topicToFind.id;
+        return t.name.indexOf(searchString) !== -1;
 			}));
 		});
 	});
@@ -51,15 +53,44 @@ describe('Topic Search', function() {
 	describe('GET /topics?q with !', function() {
 
 		it('returns existing topic', function(done) {
-			api.get('/topics?q=ind!', function(err, res) {
+      var searchString = 'ind!';
+			api.get('/topics?q=' + searchString, function(err, res) {
 				var searchResults = JSON.parse(res.body);
 				assert.ok(_.any(searchResults, function(t) {
-					return t.id === topicToFind.id;
+          return t.name.indexOf(searchString) !== -1;
 				}));
 				done();
 			});
 		});
 
 	});
+
+  describe('GET /topics?q with query that will return over 10 results', function() {
+    
+    before(function(done) {
+      var count = 0;
+      for(var i = 0; i < 11; i++) {
+        api.post('/topics', { name: 'similar topic ' + guid.raw() }, function() {
+          count++;
+          if(count === 11) {
+            done();
+          }
+        });
+      }
+    });
+
+    it('returns 10 matching results', function(done) {
+      var searchString = 'similar';
+      api.get('/topics?q=' + searchString, function(err, res) {
+        var searchResults = JSON.parse(res.body);
+        assert.equal(searchResults.length, 10);
+        assert.ok(_.all(searchResults, function(t) {
+          return t.name.indexOf(searchString) !== -1;
+        }));
+        done();
+      });
+    });
+
+  });
 
 });
