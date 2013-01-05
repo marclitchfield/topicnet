768de694738a77dc3258adc9f7eb6adc0bb8bc74
David Rueck <drueck@gmail.com>
2013-01-05 00:02:04 -0800

    Updated resource search tests to use promises

--- test/resource-search.js
+++ test/resource-search.js
@@ -9,22 +9,25 @@ describe('Resource Search', function() {
 	var resourceToFind;
 
 	before(function(done) {
-		api.post('/resources', { title: 'Resource to find! ' + guid.raw(),
+		api.postPromise('/resources', { title: 'Resource to find! ' + guid.raw(),
 			url: 'http://example.com/Upper/' + guid.raw(), source: 'example.com',
-			verb: 'read' },
-			function(err, res) {
+			verb: 'read' })
+		.then(function(res) {
 			resourceToFind = api.parseBody(res.body);
-			done(err);
-		});
+			done();
+		})
+		.done();
 	});
 
 	describe('GET /resources?invalid', function() {
 
 		it('returns status 404', function(done) {
-			api.get('/resources?invalid', function(err, res) {
+			api.getPromise('/resources?invalid')
+			.then(function(res) {
 				assert.equal(res.statusCode, 404);
 				done();
-			});
+			})
+			.done();
 		});
 
 	});
@@ -32,7 +35,8 @@ describe('Resource Search', function() {
 	describe('GET /resources?title with exact matching title', function() {
 
 		it('returns existing resource', function(done) {
-			api.get('/resources?title=' + resourceToFind.title, function(err, res) {
+			api.getPromise('/resources?title=' + resourceToFind.title)
+			.then(function(res) {
 				var searchResults = api.parseBody(res.body);
 				assert.ok(_.all(searchResults, function(r) {
 					return r.title === resourceToFind.title;
@@ -41,7 +45,8 @@ describe('Resource Search', function() {
 					return r.id === resourceToFind.id;
 				}));
 				done();
-			});
+			})
+			.done();
 		});
 
 	});
@@ -66,7 +71,8 @@ describe('Resource Search', function() {
 	describe('GET /resources?url with exact matching url', function() {
 
 		it('returns resource ', function(done) {
-			api.get('/resources?url=' + encodeURIComponent(resourceToFind.url), function(err, res) {
+			api.getPromise('/resources?url=' + encodeURIComponent(resourceToFind.url))
+			.then(function(res) {
 				var searchResults = api.parseBody(res.body);
 				assert.ok(_.all(searchResults, function(r) {
 					return r.url === resourceToFind.url;
@@ -75,7 +81,8 @@ describe('Resource Search', function() {
 					return r.id === resourceToFind.id;
 				}));
 				done();
-			});
+			})
+			.done();
 		});
 
 	});
@@ -85,13 +92,15 @@ describe('Resource Search', function() {
 		var searchString = 'o Find!';
 
 		it('returns a resource with the search string in the title', function(done) {
-			api.get('/resources?q=' + searchString, function(err, res) {
+			api.getPromise('/resources?q=' + searchString)
+			.then(function(res) {
 				var searchResults = api.parseBody(res.body);
 				assert.ok(_.all(searchResults, function(r) {
 					return r.title.contains(searchString);
 				}));
 				done();
-			});
+			})
+			.done();
 		});
 
 	});
@@ -119,12 +128,13 @@ describe('Resource Search', function() {
 					done();
 			}
 			for(var i = 0; i < 11; i++) {
-				api.post('/resources', getResource(), postCallback);
+				api.postPromise('/resources', getResource()).then(postCallback).done();
 			}
 		});
 
 		it('returns 10 matching results', function(done) {
-			api.get('/resources?q=' + searchString, function(err, res) {
+			api.getPromise('/resources?q=' + searchString)
+			.then(function(res) {
 				var searchResults = api.parseBody(res.body);
 				assert.equal(searchResults.length, 10);
 				assert.ok(_.all(searchResults, function(t) {
@@ -133,20 +143,23 @@ describe('Resource Search', function() {
 				first5Results = searchResults.slice(0,5);
 				last5Results = searchResults.slice(5,10);
 				done();
-			});
+			})
+			.done();
 		});
 
 		describe('then GET /resources?q with the same query but p=1 and pp=5', function() {
 
 			it('returns the first 5 matching results from the previous set', function(done) {
-				api.get('/resources?q=' + searchString + '&p=1&pp=5', function(err, res) {
+				api.getPromise('/resources?q=' + searchString + '&p=1&pp=5')
+				.then(function(res) {
 					var searchResults = api.parseBody(res.body);
 					assert.equal(searchResults.length, 5);
 					for(var i = 0; i < 5; i++) {
 						assert.equal(first5Results[i].id, searchResults[i].id);
 					}
 					done();
-				});
+				})
+				.done();
 			});
 
 		});
@@ -154,14 +167,16 @@ describe('Resource Search', function() {
 		describe('then GET /resources?q with same the query but p=2 and pp=5', function() {
 
 			it('returns the last 5 matching results from the previous set', function(done) {
-				api.get('/resources?q=' + searchString + '&p=2&pp=5', function(err, res) {
+				api.getPromise('/resources?q=' + searchString + '&p=2&pp=5')
+				.then(function(res) {
 					var searchResults = api.parseBody(res.body);
 					assert.equal(searchResults.length, 5);
 					for(var i = 0; i < 5; i++) {
 						assert.equal(last5Results[i].id, searchResults[i].id);
 					}
 					done();
-				});
+				})
+				.done();
 			});
 
 		});
