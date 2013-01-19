b447eb1692021da74a39a02e700e0f510ad3aa8e
David Rueck <drueck@gmail.com>
2013-01-18 22:55:04 -0800

    Updated topic search tests to use promises

--- test/topic-search.js
+++ test/topic-search.js
@@ -9,10 +9,12 @@ describe('Topic Search', function() {
 	var topicToFind;
 
 	before(function(done) {
-		api.post('/topics', { name: 'testNode to find! ' + guid.raw() }, function(err, res) {
+		api.postPromise('/topics', { name: 'testNode to find! ' + guid.raw() })
+		.then(function(res) {
 			topicToFind = api.parseBody(res.body);
 			done();
-		});
+		})
+		.done();
 	});
 
 	describe('GET /topics?q with substring', function() {
@@ -20,10 +22,12 @@ describe('Topic Search', function() {
 		var searchResults;
 
 		before(function(done) {
-			api.get('/topics?q=' + searchString, function(err, res) {
+			api.getPromise('/topics?q=' + searchString)
+			.then(function(res) {
 				searchResults = api.parseBody(res.body);
 				done();
-			});
+			})
+			.done();
 		});
 
 		it('returns a topic containing the search string', function() {
@@ -38,10 +42,12 @@ describe('Topic Search', function() {
 		var searchResults;
 
 		before(function(done) {
-			api.get('/topics?q=' + searchString, function(err, res) {
+			api.getPromise('/topics?q=' + searchString)
+			.then(function(res) {
 				searchResults = api.parseBody(res.body);
 				done();
-			});
+			})
+			.done();
 		});
 
 		it('returns a topic containing the search string', function() {
@@ -55,13 +61,15 @@ describe('Topic Search', function() {
 
 		it('returns existing topic', function(done) {
 			var searchString = 'ind!';
-			api.get('/topics?q=' + searchString, function(err, res) {
+			api.getPromise('/topics?q=' + searchString)
+			.then(function(res) {
 				var searchResults = api.parseBody(res.body);
 				assert.ok(_.any(searchResults, function(t) {
 					return t.name.contains(searchString);
 				}));
 				done();
-			});
+			})
+			.done();
 		});
 
 	});
@@ -80,12 +88,15 @@ describe('Topic Search', function() {
 					done();
 			}
 			for(var i = 0; i < 11; i++) {
-				api.post('/topics', { name: 'similar topic ' + guid.raw() }, postCallback);
+				api.postPromise('/topics', { name: 'similar topic ' + guid.raw() })
+				.then(postCallback())
+				.done();
 			}
 		});
 
 		it('returns 10 matching results', function(done) {
-			api.get('/topics?q=' + searchString, function(err, res) {
+			api.getPromise('/topics?q=' + searchString)
+			.then(function(res) {
 				var searchResults = api.parseBody(res.body);
 				assert.equal(searchResults.length, 10);
 				assert.ok(_.all(searchResults, function(t) {
@@ -94,20 +105,23 @@ describe('Topic Search', function() {
 				first5Results = searchResults.slice(0,5);
 				last5Results = searchResults.slice(5,10);
 				done();
-			});
+			})
+			.done();
 		});
 
 		describe('then GET /topics?q with the same query but p=1 and pp=5', function() {
 
 			it('returns the first 5 matching results from the previous set', function(done) {
-				api.get('/topics?q=' + searchString + '&p=1&pp=5', function(err, res) {
+				api.getPromise('/topics?q=' + searchString + '&p=1&pp=5')
+				.then(function(res) {
 					var searchResults = api.parseBody(res.body);
 					assert.equal(searchResults.length, 5);
 					for(var i = 0; i < 5; i++) {
 						assert.equal(first5Results[i].id, searchResults[i].id);
 					}
 					done();
-				});
+				})
+				.done();
 			});
 
 		});
@@ -115,14 +129,16 @@ describe('Topic Search', function() {
 		describe('then GET /topics?q with same the query but p=2 and pp=5', function() {
 
 			it('returns the last 5 matching results from the previous set', function(done) {
-				api.get('/topics?q=' + searchString + '&p=2&pp=5', function(err, res) {
+				api.getPromise('/topics?q=' + searchString + '&p=2&pp=5')
+				.then(function(res) {
 					var searchResults = api.parseBody(res.body);
 					assert.equal(searchResults.length, 5);
 					for(var i = 0; i < 5; i++) {
 						assert.equal(last5Results[i].id, searchResults[i].id);
 					}
 					done();
-				});
+				})
+				.done();
 			});
 
 		});
