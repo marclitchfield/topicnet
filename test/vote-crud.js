8621b94f54c3881aa43081b926042ffef13f0ae8
David Rueck <drueck@gmail.com>
2013-02-22 21:59:54 -0800

    Added explicit tests for voting up and down each type of relationship

--- test/vote-crud.js
+++ test/vote-crud.js
@@ -3,96 +3,126 @@ var api = require('./helper-api.js');
 
 describe('Vote CRUD', function() {
 
-	describe("POST to /topics/:id/:rel/:toid/vote with dir: 'up'", function() {
-		
+	var setupRelationship = function(postFrom, postTo, relationshipType) {
+		return postFrom.postTopic()
+		.then(function() {
+			return postTo.postTopic();
+		})
+		.then(function() {
+			return api.post('/topics/' + postFrom.returnedTopic.id +
+				'/' + relationshipType, { toid: postTo.returnedTopic.id });
+		});
+	};
+
+	var setupResourceRelationship = function(postTopic, postResource) {
+		return postTopic.postTopic()
+		.then(function() {
+			return postResource.postResource();
+		})
+		.then(function() {
+			return api.post('/topics/' + postTopic.returnedTopic.id +
+				'/resources', { resid: postResource.returnedResource.id });
+		});
+	};
+
+	var testVoteOnTopicRelationship = function(relationshipType, voteDirection) {
+
 		var postFrom = api.request();
 		var postTo = api.request();
-		var relationshipType = 'sub';
+		var votesKey = (voteDirection === 'up') ? 'upVotes' : 'downVotes';
 		var response;
 
 		before(function(done) {
-			postFrom.postTopic()
-			.then(function() {
-				return postTo.postTopic();
-			})
-			.then(function() {
-				return api.post('/topics/' + postFrom.returnedTopic.id +
-					'/' + relationshipType, { toid: postTo.returnedTopic.id });
-			})
+			setupRelationship(postFrom, postTo, relationshipType)
 			.then(function() {
 				return api.post('/topics/' + postFrom.returnedTopic.id +
 					'/' + relationshipType + '/' + postTo.returnedTopic.id + '/vote',
-					{ dir: 'up' });	
+					{ dir: voteDirection });
 			})
 			.then(function(res) {
 				response = res;
 				done();
 			})
 			.done();
-
 		});
 
 		it('returns status 200', function() {
 			assert.equal(response.statusCode, 200);
 		});
 
-		it('increments the upVotes on the specified relationship by 1', function(done) {
+		it('increments the ' + votesKey + ' on the specified relationship by 1', function(done) {
 			api.get('/topics/' + postFrom.returnedTopic.id +
 				'/' + relationshipType + '/' + postTo.returnedTopic.id)
 			.then(function(res) {
 				var rel = JSON.parse(res.body);
-				assert.equal(rel.upVotes, 1);
+				assert.equal(rel[votesKey], 1);
 				done();
 			})
 			.done();
 		});
 
+	};
+
+	var testVoteOnResourceRelationship = function(voteDirection) {
+
+		var postTopic = api.request();
+		var postResource = api.request();
+		var response;
+		var votesKey = (voteDirection === 'up') ? 'upVotes' : 'downVotes';
+
+		before(function(done) {
+			setupResourceRelationship(postTopic, postResource)
+			.then(function(done) {
+				return api.post('/topics/' + postTopic.returnedTopic.id +
+					'/resources/' + postResource.returnedResource.id + '/vote',
+					{ dir: voteDirection });
+			})
+			.then(function(res) {
+				response = res;
+				done();
+			})
+			.done();
+		});
+
+		it('returns status 200', function() {
+			assert.equal(response.statusCode, 200);
+		});
+
+		it('increments the ' + votesKey + ' on the specified relationship by 1', function(done) {
+			api.get('/topics/' + postTopic.returnedTopic.id +
+				'/resources/' + postResource.returnedResource.id)
+			.then(function(res) {
+				var rel = JSON.parse(res.body);
+				assert.equal(rel[votesKey], 1);
+				done();
+			})
+			.done();
+		});
+
+	};
+
+	describe("POST to /topics/:id/next/:toid/vote with dir: 'up'", function() {
+		testVoteOnTopicRelationship('next', 'up');
 	});
 
-	describe("POST to /topics/:id/:rel/:toid/vote with dir: 'down'", function() {
-		
-		var postFrom = api.request();
-		var postTo = api.request();
-		var relationshipType = 'next';
-		var response;
+	describe("POST to /topics/:id/next/:toid/vote with dir: 'down'", function() {
+		testVoteOnTopicRelationship('next', 'down');
+	});
 
-		before(function(done) {
-			postFrom.postTopic()
-			.then(function() {
-				return postTo.postTopic();
-			})
-			.then(function() {
-				return api.post('/topics/' + postFrom.returnedTopic.id +
-					'/' + relationshipType, { toid: postTo.returnedTopic.id });
-			})
-			.then(function() {
-				return api.post('/topics/' + postFrom.returnedTopic.id +
-					'/' + relationshipType + '/' + postTo.returnedTopic.id + '/vote',
-					{ dir: 'down' });	
-			})
-			.then(function(res) {
-				response = res;
-				done();
-			})
-			.done();
+	describe("POST to /topics/:id/sub/:toid/vote with dir: 'up'", function() {
+		testVoteOnTopicRelationship('sub', 'up');
+	});
 
-		});
+	describe("POST to /topics/:id/sub/:toid/vote with dir: 'down'", function() {
+		testVoteOnTopicRelationship('sub', 'down');
+	});
 
-		it('returns status 200', function() {
-			assert.equal(response.statusCode, 200);
-		});
+	describe("POST to /topics/:id/resources/:resid/vote with 'up'", function() {
+		testVoteOnResourceRelationship('up');
+	});
 
-		it('increments the downVotes on the specified relationship by 1', function(done) {
-			api.get('/topics/' + postFrom.returnedTopic.id +
-				'/' + relationshipType + '/' + postTo.returnedTopic.id)
-			.then(function(res) {
-				var rel = JSON.parse(res.body);
-				assert.equal(rel.downVotes, 1);
-				done();
-			})
-			.done();
-		});
-
+	describe("POST to /topics/:id/resources/:resid/vote with 'down'", function() {
+		testVoteOnResourceRelationship('down');
 	});
 
 });
