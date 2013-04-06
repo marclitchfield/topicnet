c8649d432738376155502cc88486786378a3702e
marclitchfield <marclitchfield@gmail.com>
2013-04-05 20:40:21 -0700

    Removing that last change. Experiment success

--- test/service/root-topics.js
+++ test/service/root-topics.js
@@ -46,8 +46,6 @@ describe('Root Topics', function() {
 			it('returns all root topics including our topic', function() {
 				var rootTopics = api.parseBody(rootTopicsResponse.body);
 
-				console.log('RootTopics:' + rootTopics.length);
-
 				assert.ok(_.any(rootTopics, function(t) {
 					return t.id === postTopic.returnedData.id;
 				}));
