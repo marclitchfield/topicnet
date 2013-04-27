edf1568b6a8e3ba3ff54f3303e275dca5db3f8ec
marclitchfield <marclitchfield@gmail.com>
2013-04-26 21:35:01 -0700

    Refactor topic detail tests. Much cleaner!

--- test/public/controllers/topic-detail-controller.js
+++ test/public/controllers/topic-detail-controller.js
@@ -5,19 +5,17 @@ describe('TopicDetailController', function() {
 		var scope, httpBackend;
 		var topic = { id: 1, name: 'topic', resources: [] };
 
-		beforeEach(inject(function($rootScope, $httpBackend) {
+		beforeEach(inject(function($rootScope, $httpBackend, $controller) {
 			scope = $rootScope.$new();
 			httpBackend = $httpBackend;
+
+			var params = { topicId: topic.id };
+			httpBackend.expectGET('/topics/1').respond(topic);
+			$controller('TopicDetailController', { $scope: scope, $routeParams: params });
+			httpBackend.flush();
 		}));
 
-		describe('when constructed with topicId parameter', function() {
-			beforeEach(inject(function($controller) {
-				var params = { topicId: topic.id };
-				httpBackend.expectGET('/topics/1').respond(topic);
-				$controller('TopicDetailController', { $scope: scope, $routeParams: params });
-				httpBackend.flush();
-			}));
-
+		describe('when topic detail is loaded', function() {
 			it('should request the topic from the server',  function() {
 				httpBackend.verifyNoOutstandingExpectation();
 			});
@@ -35,11 +33,6 @@ describe('TopicDetailController', function() {
 
 		describe('when update is called', function() {
 			beforeEach(inject(function($controller) {
-				var params = { topicId: topic.id };
-				httpBackend.expectGET('/topics/1').respond(topic);
-				$controller('TopicDetailController', { $scope: scope, $routeParams: params });
-				httpBackend.flush();
-				
 				httpBackend.expectPUT('/topics/1', { name: 'edited' }).respond(200, {});
 				scope.editedTopicName = 'edited';
 				scope.update();
@@ -58,47 +51,36 @@ describe('TopicDetailController', function() {
 		describe('when a resource is removed from a topic', function() {
 			beforeEach(inject(function($controller) {
 				topic.resources = [ {id: 8}, {id: 9} ];
-				var params = { topicId: topic.id };
-				httpBackend.expectGET('/topics/1').respond(topic);
-				$controller('TopicDetailController', { $scope: scope, $routeParams: params });
-				httpBackend.flush();
-
 				httpBackend.expectPOST('/topics/1/resources/8/hide').respond(200, {});
 				scope.hideResource(8);
 				httpBackend.flush();
 			}));
 
-			it('should remove the resource from the list', function() {
-				expect(topic.resources).toEqual([{ id: 9 }]);
-			});
-
 			it('should tell the service to hide the resource on the current topic', function() {
 				httpBackend.verifyNoOutstandingExpectation();
+			});
+
+			it('should remove the resource from the list', function() {
+				expect(topic.resources).toEqual([{ id: 9 }]);
 			});
 		});
 
 		describe('when a resource is dragged onto a related topic', function() {
 			beforeEach(inject(function($controller) {
 				topic.resources = [ {id: 8}, {id: 9} ];
-				var params = { topicId: topic.id };
-				httpBackend.expectGET('/topics/1').respond(topic);
-				$controller('TopicDetailController', { $scope: scope, $routeParams: params });
-				httpBackend.flush();
-
 				httpBackend.expectPOST('/topics/1/resources/8/hide').respond(200, {});
 				httpBackend.expectPOST('/topics/1/sub/999/affirm').respond(200, {});
 				scope.moveResource(8, 'sub', 999);
 				httpBackend.flush();
 			}));
 
-			it('should remove the resource from the list', function() {
-				expect(topic.resources).toEqual([{ id: 9 }]);
-			});
-
 			it('should tell the service to hide the resource on the current topic and affirm it on the target topic', function() {
 				httpBackend.verifyNoOutstandingExpectation();
 			});
 
+			it('should remove the resource from the list', function() {
+				expect(topic.resources).toEqual([{ id: 9 }]);
+			});
 		});
 	});
 });
