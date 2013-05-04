b6ba35a7040e58a8cc9b675623b0684244c13eef
marclitchfield <marclitchfield@gmail.com>
2013-05-04 11:12:06 -0700

    Passed topic-service unit tests

--- test/service/lib/topic-service-test.js
+++ test/service/lib/topic-service-test.js
@@ -1,8 +1,10 @@
 var assert = require('assert');
 var Q = require('q');
 var graph = require('../../../service/lib/graph');
+var topicnetGraph = require('../../../service/lib/topicnet-graph').create(graph);
 var topicService = require('../../../service/lib/topic-service');
 var sinon = require('sinon');
+
 
 describe('Topic Service', function() {
 
@@ -10,23 +12,50 @@ describe('Topic Service', function() {
 
 		describe('when the link does not already exist', function() {
 
-			var mockGraph;
-			var service;
+			var mockGraph, service;
 
-			before(function(done) {
-				mockGraph = sinon.mock(graph);
-				service = topicService.createService(mockGraph);
-				done();
+			before(function() {
+				mockGraph = sinon.mock(topicnetGraph);
+				service = topicService.createService(graph, mockGraph.object);
+				mockGraph.expects('getResourceRelationship').withArgs(1,2)
+					.returns(Q.resolve(undefined));
+				mockGraph.expects('linkResource').withArgs(1,2);
 			});
-
-			it('should link a resource to a topic', function() {
-				mockGraph.expects('queryRelationship').withArgs(1,2,'resources')
-				.returns(Q.resolve([]));
+			
+			it('should link a resource to a topic', function(done) {
+				service.linkResource(1,2)
+				.then(function(relationship) {
+					assert.ok(relationship);
+					mockGraph.verify();
+					done();
+				})
+				.done();
 			});
 
 		});
 
+		describe('when the link already exists', function() {
 
+			var mockGraph, service;
+
+			before(function() {
+				mockGraph = sinon.mock(topicnetGraph);
+				service = topicService.createService(graph, mockGraph.object);
+				mockGraph.expects('getResourceRelationship').withArgs(1,2)
+					.returns(Q.resolve({id:123}));
+			});
+
+			it('should return an error', function(done) {
+				service.linkResource(1,2)
+				.fail(function(error) {
+					assert.equal('duplicate', error.name);
+					mockGraph.verify();
+					done();
+				})
+				.done();
+			});
+
+		});
 	});
 
 });
