40847f17df44908a9f5710676c98762704f9db05
Ubuntu <marclitchfield@gmail.com>
2013-05-26 07:43:05 -0700

    Revert "Change reference to require mocha"
    This reverts commit 420e421e08af3be56930d34948c0cdd3229e7a78.

--- test/service/test-utils.js
+++ test/service/test-utils.js
@@ -1,6 +1,6 @@
 var assert = require('assert');
 
-require('mocha-as-promised')(require('mocha'));
+require("mocha-as-promised")(require("grunt-mocha-test/node_modules/mocha"));
 
 String.prototype.contains = function(searchString) {
 	return this.toLowerCase().indexOf(searchString.toLowerCase()) !== -1;
