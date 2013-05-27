8481659fa824270b24182fe8aed84018453ba928
Ubuntu <marclitchfield@gmail.com>
2013-05-27 13:54:18 -0700

    Got mocha-as-promised working on linux!
    Had to write a crazy function to find the loaded instance of mocha.
    Could consider forking mocha-as-promised and using this technique
    to locate mocha. Probably worthy of a pull request.

--- test/service/test-utils.js
+++ test/service/test-utils.js
@@ -1,5 +1,25 @@
 var assert = require('assert');
 
+function findMocha(module) {
+	if (module.children) {
+		for (var i=0; i<module.children.length; i++) {
+			var childModule = module.children[i];
+
+			if (childModule.id && childModule.id.indexOf('/mocha/lib/mocha.js') > -1) {
+				return childModule;
+			}
+
+			var found = findMocha(childModule);
+			if (found) {
+				return found;
+			}
+		}
+	}
+}
+
+var mocha = findMocha(require.main).exports;
+require('mocha-as-promised')(mocha);
+
 String.prototype.contains = function(searchString) {
 	return this.toLowerCase().indexOf(searchString.toLowerCase()) !== -1;
 };
