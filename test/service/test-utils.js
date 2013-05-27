667684e4383f1b55319dc41b626074c6b42e798e
Ubuntu <marclitchfield@gmail.com>
2013-05-27 14:42:24 -0700

    Integrated with fork of mocha-as-promised with improved technique for finding the running mocha instance

--- test/service/test-utils.js
+++ test/service/test-utils.js
@@ -1,25 +1,6 @@
 var assert = require('assert');
 
-function findMocha(module) {
-	if (module.children) {
-		for (var i=0; i<module.children.length; i++) {
-			var childModule = module.children[i];
-
-			var suffix = 'mocha.js';
-			if (childModule.id && childModule.id.indexOf(suffix, childModule.id.length - suffix.length) > -1) {
-				return childModule;
-			}
-
-			var found = findMocha(childModule);
-			if (found) {
-				return found;
-			}
-		}
-	}
-}
-
-var mocha = findMocha(require.main).exports;
-require('mocha-as-promised')(mocha);
+require('mocha-as-promised')();
 
 String.prototype.contains = function(searchString) {
 	return this.toLowerCase().indexOf(searchString.toLowerCase()) !== -1;
